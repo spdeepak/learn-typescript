41be37921be83af0852cf56ec6078efab80cafc0
spdeepak <spdeepak@outlook.com>
2019-10-05 19:34:55 +0200

    transform ObjectId to hex String

--- src/entity/Book.ts
+++ src/entity/Book.ts
@@ -1,8 +1,10 @@
+import { Transform } from 'class-transformer'
 import { Column, Entity, ObjectID, ObjectIdColumn } from 'typeorm'
 
 @Entity()
 export class Book {
   @ObjectIdColumn()
+  @Transform((id: ObjectID) => id.toHexString(), { toPlainOnly: true })
   public _id: ObjectID
   @Column()
   public title: string
