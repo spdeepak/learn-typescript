1731be7e94ee501922c904a97ace57cfb75177b7
spdeepak <spdeepak@outlook.com>
2019-10-05 08:23:28 +0200

    tslint

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,44 +1,44 @@
-import { getManager } from "typeorm";
 import {
-  JsonController,
-  Get,
-  Post,
-  Body,
   BadRequestError,
+  Body,
+  Get,
+  JsonController,
   NotFoundError,
-  Param
-} from "routing-controllers";
-import { Book } from "../entity/Book";
-import logger from "..";
+  Param,
+  Post,
+} from 'routing-controllers'
+import { getManager } from 'typeorm'
+import logger from '..'
+import { Book } from '../entity/Book'
 
-@JsonController("")
+@JsonController('')
 export class BookController {
-  bookRepository = getManager().getRepository(Book);
+  public bookRepository = getManager().getRepository(Book)
 
-  @Get("/books")
+  @Get('/books')
   public async getAllBooks() {
     try {
-      const books = await this.bookRepository.find();
-      return books;
+      const books = await this.bookRepository.find()
+      return books
     } catch (_err) {
-      throw new NotFoundError("Books Not found");
+      throw new NotFoundError('Books Not found')
     }
   }
 
-  @Get("/book/:id")
-  public getBookByID(@Param("id") id: string) {
-    return this.bookRepository.findOne(id);
+  @Get('/book/:id')
+  public getBookByID(@Param('id') id: string) {
+    return this.bookRepository.findOne(id)
   }
 
-  @Post("/book")
+  @Post('/book')
   public async createBook(@Body({ required: true }) book: Book) {
     try {
-      logger.info(`Saving book: ${book}`);
-      logger.info("::", JSON.stringify(book));
-      return await this.bookRepository.save(book);
+      logger.info(`Saving book: ${book}`)
+      logger.info('::', JSON.stringify(book))
+      return await this.bookRepository.save(book)
     } catch (err) {
-      logger.error(`Error saving book ${err}`);
-      throw new BadRequestError("Error saving Book");
+      logger.error(`Error saving book ${err}`)
+      throw new BadRequestError('Error saving Book')
     }
   }
 }
